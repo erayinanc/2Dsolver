4c0133301110d138815c48f5fd0c66c681933c5f
Eray Inanc <eray.inanc@uni-due.de>
2018-06-07 14:44:40 +0200

    Update PoissonSolver.m

--- PoissonSolver.m
+++ PoissonSolver.m
@@ -1,11 +1,11 @@
 %% PoissonSolver
 %  Solve (approximately) the Poisson equation for the pressure correction.
-%  Find a field p such that div(grad(p)) = div(u)
+%  Find a field p such that div(grad(p)) = div
+function [p,nIt,eps]=PoissonSolver(div,dt,omega,nItMax,epsMax) 
+    global Ima Jma dx nG Ifim Ifi Ifip Ilam Ila Ilap Jfim Jfi Jfip Jlam Jla Jlap;
 
-function [p,nIt,eps]=PoissonSolver(div,rhoPhi,rho,dt,dx,omega,nItMax,epsMax) 
     % Initialisation
-    [Ima,Jma]=size(rhoPhi.rhoU);
-    p = zeros(Ima,Jma);
+    p = zeros(Ima+2*nG,Jma+2*nG);
 
     % Loop over iteration steps. 
     eps = 1.0E+9; nIt = 0;
@@ -17,44 +17,14 @@ function [p,nIt,eps]=PoissonSolver(div,rhoPhi,rho,dt,dx,omega,nItMax,epsMax)
             pold = p;
 
             % Compute new pressure with Jacobi point iteration
-            p(2:Ima-1,2:Jma-1) = 0.25 * (pold(3:Ima,2:Jma-1) + pold(1:Ima-2,2:Jma-1) ...
-                                       + pold(2:Ima-1,3:Jma) + pold(2:Ima-1,1:Jma-2) ...
-                                      - omega.*(div(2:Ima-1,2:Jma-1)*rho*dx./dt) );
-
+            p(Ifi:Ila,Jfi:Jla) = 0.25 * (pold(Ifip:Ilap,Jfi:Jla) + pold(Ifim:Ilam,Jfi:Jla) ...
+                                       + pold(Ifi:Ila,Jfip:Jlap) + pold(Ifi:Ila,Jfim:Jlam) ...
+                                       - omega*(div(Ifi:Ila,Jfi:Jla)*dx/dt) );
+                                  
             % Set zero gradient BC at the inlet and fixed value of zero at all            
-            p(1,:) = p(2,:); p(Ima,:) = -p(Ima-1,:);
-            p(:,1) = p(:,2); p(:,Jma) = -p(:,Jma-1);
-                  
+            p(Ifim,:) = p(Ifi,:); p(Ilap,:) = 0;
+            
             % Compute maximum error
             eps = max (max (abs (p-pold)));  
     end
 end   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

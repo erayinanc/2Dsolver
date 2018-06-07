3a1e836080c0628713ebb29182a94fafd675e0d9
Eray Inanc <eray.inanc@uni-due.de>
2018-06-07 14:45:00 +0200

    Update calcDivergence.m

--- calcDivergence.m
+++ calcDivergence.m
@@ -1,11 +1,10 @@
 %% calcDivergence
 %  calculates the divergence of the velocity field, div = du/dx + dv/dy
+function [div]=calcDivergence(rhoU,rhoV)
+    global Ima Jma nG Ifim Ifi Ifip Ilam Ila Ilap Jfim Jfi Jfip Jlam Jla Jlap;
 
-function [div]=calcDivergence(U,V)
-    % Initialisation
-    [Ima,~] = size(U);
-    [~,Jma] = size(V);
-    div = zeros(Ima,Jma);
-    div(2:Ima-1,2:Jma-1) = ( U(3:Ima,2:Jma-1)-U(1:Ima-2,2:Jma-1)...
-                           + V(2:Ima-1,3:Jma)-V(2:Ima-1,1:Jma-2) );
+    % Calculation
+    div = zeros(Ima+2*nG,Jma+2*nG);
+    div(Ifi:Ila,Jfi:Jla) = .5 * ( rhoU(Ifip:Ilap,Jfi:Jla) - rhoU(Ifim:Ilam,Jfi:Jla)...
+                                + rhoV(Ifi:Ila,Jfip:Jlap) - rhoV(Ifi:Ila,Jfim:Jlam) );
 end

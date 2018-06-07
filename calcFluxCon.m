3cbcbcdcbd208530cb713d20ae0b64cf1c7bbe11
Eray Inanc <eray.inanc@uni-due.de>
2018-06-07 14:45:34 +0200

    Update calcFluxCon.m

--- calcFluxCon.m
+++ calcFluxCon.m
@@ -2,37 +2,32 @@
 %  fluxConX, fluxConY over the cell surfaces as a function of
 %  a conserved scalar field Phi from the fluxes U and V 
 %  with different schemes, DDS will never work!
-
-function [fluxConX,fluxConY]=calcFluxCon(Phi,fluxU,fluxV,dt,deltaX,scheme)
-    % Initialisation
-    [Ima,Jma]=size(Phi);
+function [fluxConX,fluxConY]=calcFluxCon(Phi,fluxU,fluxV,dt,scheme)
+    global Ima Jma dx Ifim Ifi Ila Ilap Jfim Jfi Jla Jlap;
 
     % Calculation
     switch(scheme)
         case 0 % CDS
-            fluxConX = 0.5 .* fluxU(1:Ima-1,:).*(Phi(1:Ima-1,:)+Phi(2:Ima,:));
-            fluxConY = 0.5 .* fluxV(:,1:Jma-1).*(Phi(:,1:Jma-1)+Phi(:,2:Jma));
+            fluxConX = 0.5 .* fluxU(Ifim:Ila,:).*(Phi(Ifim:Ila,:)+Phi(Ifi:Ilap,:));
+            fluxConY = 0.5 .* fluxV(:,Jfim:Jla).*(Phi(:,Jfim:Jla)+Phi(:,Jfi:Jlap));
         case 1 % UDS
-            fluxConX = max(0,fluxU(1:Ima-1,:)).*(Phi(1:Ima-1,:)) + ...
-                       min(0,fluxU(1:Ima-1,:)).*(Phi(2:Ima,:));
-            fluxConY = max(0,fluxV(:,1:Jma-1)).*(Phi(:,1:Jma-1)) + ...
-                       min(0,fluxV(:,1:Jma-1)).*(Phi(:,2:Jma));
-        case 2 % DDS
-            fluxConX = fluxU(1:Ima-1,:).*(Phi(2:Ima,:));
-            fluxConY = fluxV(:,1:Jma-1).*(Phi(:,2:Jma));
-        case 3 % UDCDS         
+            fluxConX = max(0,fluxU(Ifim:Ila,:)).*(Phi(Ifim:Ila,:)) + ...
+                       min(0,fluxU(Ifim:Ila,:)).*(Phi(Ifi:Ilap,:));
+            fluxConY = max(0,fluxV(:,Jfim:Jla)).*(Phi(:,Jfim:Jla)) + ...
+                       min(0,fluxV(:,Jfim:Jla)).*(Phi(:,Jfi:Jlap));
+        case 2 % UDCDS         
             % Calculate weigths
-            ww(1:Ima-1,:) = 0.5.*(fluxU(1:Ima-1,:)*dt/deltaX + 1);
-            we(2:Ima,:)   = 1.0-ww(1:Ima-1,:);
-            ws(:,1:Jma-1) = 0.5.*(fluxV(:,1:Jma-1)*dt/deltaX + 1);
-            wn(:,2:Jma)   = 1.0-ws(:,1:Jma-1);
+            ww(Ifim:Ila,:) = 0.5.*(fluxU(Ifim:Ila,:)*dt/dx + 1);
+            we(Ifi:Ilap,:) = 1.0-ww(Ifim:Ila,:);
+            ws(:,Jfim:Jla) = 0.5.*(fluxV(:,Jfim:Jla)*dt/dx + 1);
+            wn(:,Jfi:Jlap) = 1.0-ws(:,Jfim:Jla);
 
             % Calculation
-            fluxConX = fluxU(1:Ima-1,:)...
-                                .* ( ww(1:Ima-1,:).*Phi(1:Ima-1,:)...
-                                   + we(2:Ima,:)  .*Phi(2:Ima,:) );
-            fluxConY = fluxV(:,1:Jma-1)...
-                                .* ( ws(:,1:Jma-1).*Phi(:,1:Jma-1)...
-                                   + wn(:,2:Jma)  .*Phi(:,2:Jma) );
+            fluxConX = fluxU(Ifim:Ila,:)...
+                                .* ( ww(Ifim:Ila,:).*Phi(Ifim:Ila,:)...
+                                   + we(Ifi:Ilap,:).*Phi(Ifi:Ilap,:) );
+            fluxConY = fluxV(:,Jfim:Jla)...
+                                .* ( ws(:,Jfim:Jla).*Phi(:,Jfim:Jla)...
+                                   + wn(:,Jfi:Jlap).*Phi(:,Jfi:Jlap) );
     end
 end
